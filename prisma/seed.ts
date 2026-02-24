1d69137f4b4e18fd3ef501e49527dcf0c8bfb298
Hafidz Siddiq <hafidzsddq03@email.com>
2026-02-24 16:37:40 +0700

    balik lagi njirr

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,8 +3,6 @@ import { MemberRole, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.account.deleteMany();
-
   await prisma.account.upsert({
     where: { npm: "2214373011" },
     update: {
@@ -37,7 +35,7 @@ async function main() {
     },
   });
 
-  await prisma.account.upsert({
+   await prisma.account.upsert({
     where: { npm: "2214373062" },
     update: {},
     create: {
